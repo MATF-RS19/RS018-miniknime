9b18289ffb109aab245ea903f8e7d98503e0c6cb
Stefan <stefan77403@gmail.com>
2019-01-07 23:58:13 +0100

    Working on GUI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,13 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->listWidget->addItem("Linear regression");
+    ui->listWidget->addItem("Kernel K-Means Clustering");
+    ui->listWidget->addItem("Kernel Centroid");
+    ui->listWidget->addItem("Neural Network");
+    ui->listWidget->addItem("SVM Rank");
+    ui->listWidget->addItem("KRR Classification");
+
 }
 
 MainWindow::~MainWindow()
