994f59b876e5b11c5051084753b6c85178ced2ff
Stefan <stefan77403@gmail.com>
2019-01-13 00:17:17 +0100

    Implementing forms for node dialogs

---
+++ csvreaderdialog.h
@@ -0,0 +1,26 @@
+#ifndef CSVREADERDIALOG_H
+#define CSVREADERDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class CsvReaderDialog;
+}
+
+class CsvReaderDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit CsvReaderDialog(QWidget *parent = nullptr);
+    ~CsvReaderDialog();
+
+private slots:
+
+    void on_browseButton_clicked();
+
+private:
+    Ui::CsvReaderDialog *ui;
+};
+
+#endif // CSVREADERDIALOG_H
